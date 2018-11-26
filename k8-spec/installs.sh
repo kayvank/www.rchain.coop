8f88110574771d5633526a3de0fcf957130e6d88
kayvank <kayvan@q2io.com>
2018-11-26 08:35:30 -0800

    incomplete. broken deploy

--- k8-spec/installs.sh
+++ k8-spec/installs.sh
@@ -9,7 +9,12 @@ kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admi
 
 kubectl patch deploy --namespace kube-system tiller-deploy -p '{"spec":{"template":{"spec":{"serviceAccount":"tiller"}}}}'
 
-kubectl create configmap AppNameConfig --from-literal API_HOST=http://api-host --from-literal STATIC_HOST=$STATIC_HOST
+kubectl create -f ./tiller-clusterrolebinding.yaml
+
+helm install  --name cert-manager \
+     -f ./cert-manager-setup/cert-manager-values.yaml \
+     stable/cert-manager
+
 
 helm install \
      --name cert-manager \
@@ -17,12 +22,19 @@ helm install \
      --set rbac.create=true \
      stable/cert-manager
 
+# helm install \
+#      --name cert-manager \
+#      --namespace kube-system \
+#      --set ingressShim.defaultIssuerName=letsencrypt-staging \
+#      --set ingressShim.defaultIssuerKind=ClusterIssuer \
+#      stable/cert-manager 
+
 helm install \
   --name cert-manager \
   --namespace kube-system \
   --set ingressShim.defaultIssuerName=letsencrypt-staging \
   --set ingressShim.defaultIssuerKind=ClusterIssuer \
-  --set rbac.create=false \
+  --set rbac.create=true \
   stable/cert-manager 
 
 kubectl create -f cert-manager-setup/letsencrypt-clusterissuer-staging.yaml
@@ -30,5 +42,4 @@ kubectl create -f cert-manager-setup/letsencrypt-clusterissuer-staging.yaml
 helm install stable/nginx-ingress \
   --name coop-nginx \
   --set rbac.create=true \
-  --namespace kube-system \
-  --set ingressShim.extraArgs='{--default-issuer-name=letsencrypt-stable,--default-issuer-kind=ClusterIssuer}'
+  --namespace kube-system 
